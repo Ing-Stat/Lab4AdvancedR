e708b987044361db9da9cc79765cae4b6b7c3086
Kristina Levina <existentialantiproton@protonmail.com>
2021-09-15 10:49:49 +0200

    create the package skeleton

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+hello <- function(){
+  
+  #' Saying Hello
+  #'
+  #' @export
+  #' 
+  #' @return Welcoming message.
+  #' @examples
+  #' hello()
+  #' @source \url{}
+
+  return("Behave yourself, people!")
+}
