e0b8413c286dc43088f2ecb2cac40c7268596c56
Your Nameinge <ingemar.sjostrom@telia.com>
2021-09-16 13:12:50 +0200

    Coeff function

--- R/coeff.R
+++ R/coeff.R
@@ -1,3 +1,5 @@
-coeff <- function(){
-  Coeff
+coeff <- function(Coeff) {
+  
+  Coeff <- round(Coeff, digits=3)
+  return (c("Coefficients X0" = Coeff[1], "X1" = Coeff[2], "X2" = Coeff[3], "X3" = Coeff[4]))
 }
